d0e696476c5b5aa21ce11d4808240cbda3e9017b
Devankar <devankarraj@gmail.com>
2024-05-29 18:08:34 +0530

    Updated field in models.py

--- Problem/models.py
+++ Problem/models.py
@@ -5,17 +5,17 @@ from Authentication.models import detail
 class question(models.Model):
     name = models.CharField(max_length=100)
     difficulty = models.DecimalField(max_digits=1, decimal_places=0)
-    description = models.CharField(max_length=2000)
-    in_format = models.CharField(max_length=1000, null=True)
-    out_format = models.CharField(max_length=1000, null=True)
+    description = models.TextField(null=True)
+    in_format = models.TextField(null=True)
+    out_format = models.TextField(null=True)
 
     def __str__(self):
         return self.name
 
 class testcase(models.Model):
     question = models.ForeignKey(question, on_delete=models.CASCADE)
-    inputs = models.CharField(max_length=2000)
-    outputs = models.CharField(max_length=2000)
+    inputs = models.TextField(null=True)
+    outputs = models.TextField(null=True)
 
     def __str__(self):
         return self.question.name 
